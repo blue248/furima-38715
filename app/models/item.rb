0d4d1b45394476ceaac4d1390cff09e174dc61f3
blue248 <a.k0reha.mar1m0@gmail.com>
2022-12-21 15:58:51 +0900

    デバッグコード削除、itemモデルの重複したアソシエーション削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,6 @@ class Item < ApplicationRecord
   belongs_to :shipping_charge
   belongs_to :prefecture
   belongs_to :shipping_day
-  belongs_to :user
 
   validates :image, :name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_day_id,
             presence: true
