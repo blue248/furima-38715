17af64e8c47504d52b6ba9c05c24de29c1a7adcc
blue248 <a.k0reha.mar1m0@gmail.com>
2022-12-22 17:58:17 +0900

    before_action集約、ログイン中の出品者本人のみが削除できる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
@@ -35,8 +35,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    if @item.user_id == current_user.id
+      @item.destroy
+    end
     redirect_to root_path
   end
 
