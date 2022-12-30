95abdbdc949be753af9ba848463ffa1d984c0678
blue248 <a.k0reha.mar1m0@gmail.com>
2022-12-30 15:11:57 +0900

    日本語対応

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,14 +13,14 @@ class Item < ApplicationRecord
   validates :image, :name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_day_id,
             presence: true
 
-  validates :price, presence: true, numericality: { message: 'is invalid. Input half-width characters.' }
+  validates :price, presence: true, numericality: { message: 'は半角数字で入力してください' }
   validates :price,
             numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
-                            message: 'is out of setting range' }
+                            message: 'が設定範囲外です' }
 
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_charge_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id, numericality: { other_than: 1, message: 'を選択してください' }
+  validates :condition_id, numericality: { other_than: 1, message: 'を選択してください' }
+  validates :shipping_charge_id, numericality: { other_than: 1, message: 'を選択してください' }
+  validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
+  validates :shipping_day_id, numericality: { other_than: 1, message: 'を選択してください' }
 end
--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -3,11 +3,11 @@ class OrderDestination
   attr_accessor :post_code, :prefecture_id, :city, :block, :building_name, :phone_no, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :city, presence: { message: "can't be blank" }
-    validates :block, presence: { message: "can't be blank" }
-    validates :phone_no, format: { with: /\A\d{10,11}\z/, message: 'Input only number' }
+    validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: '半角数字とハイフンを用いて入力してください (例： 123-4567)' }
+    validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
+    validates :city, presence: { message: 'を入力してください' }
+    validates :block, presence: { message: 'を入力してください' }
+    validates :phone_no, format: { with: /\A\d{10,11}\z/, message: 'は半角数字のみで入力してください' }
     validates :user_id
     validates :item_id
     validates :token
